2a04c1ecf6798e607afd3d4e25f1d1afb01a003d
Minras <andrey.shchurkov@gmail.com>
2015-12-23 16:57:52 +0100

    js logic moved to js files from html

--- www/js/index.js
+++ www/js/index.js
@@ -4,8 +4,32 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.init_page_behaviour();
         this.log.add("Application initialization finished");
     },
+
+    init_page_behaviour: function() {
+        $("#page-tabs").tabs({
+            active: 1,
+            activate: function(event, ui) {
+                if ('tab-logs' == ui.newPanel.attr('id')) {
+                    var container = $('#tab-logs-content').empty();
+                    var i = app.log.list().length;
+                    if (!i) {
+                        container.html($('#log-msg-empty').html());
+                        return;
+                    }
+                    var tpl = $('#log-msg-template').html();
+                    for(i=i-1; i>=0; i--) {
+                        var el = $('<div />').loadTemplate($("#log-msg-template"), app.log.get(i));
+                        container.append(el.html());
+                    }
+
+                }
+            }
+        });
+    },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -13,6 +37,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+
     // Phonegap is now ready...
     onDeviceReady: function() {
         this.log.add("Device is ready");
